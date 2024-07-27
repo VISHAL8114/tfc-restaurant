0e8811b5c2d8c48fa408bdbf8b710c3f6c60749d
VISHAL8114 <vishalt8114@gmail.com>
2024-07-27 23:13:04 +0530

    tailwind changes

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,30 +1,12 @@
 const Shimmer = () => {
     return (
-      <div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
+      <div className="flex flex-wrap justify-center gap-x-10 gap-y-6 mt-28">
+        {Array.from({ length: 24 }).map((_, index) => (
+          <div
+            key={index}
+            className="w-52 h-[450px] bg-shimmer rounded-xl cursor-pointer animate-shimmer"
+          ></div>
+        ))}
       </div>
     );
   };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,19 @@ module.exports = {
     extend: {
       colors: {
         mediumGreen: 'rgb(0, 128, 0)', // Define your custom color
-      },
+  shimmer: '#efefef',
+
+      },animation: {
+        shimmer: 'shimmer 3s infinite',
+      },keyframes: {
+        shimmer: {
+          '0%': { backgroundPosition: '-1000px 0' },
+          '100%': { backgroundPosition: '1000px 0' },
+        },
+      }
     },
   },
   plugins: [],
 }
+
 
